9d9b3c9e4b923232d347ef1dedf7b68c8af77906
Tajamul Wani <thetj4054@gmail.com>
2026-08-18 16:03:42 +0000

    feat: add index.js barrel export for utils directory

---
+++ utils/index.js
@@ -0,0 +1,46 @@
+/**
+ * Central export file for all utility modules in utils/.
+ */
+const arrayUtils = require('./arrayUtils');
+const asyncUtils = require('./asyncUtils');
+const collectionUtils = require('./collectionUtils');
+const colorUtils = require('./colorUtils');
+const cryptoUtils = require('./cryptoUtils');
+const dateFormatter = require('./dateFormatter');
+const envUtils = require('./envUtils');
+const fileUtils = require('./fileUtils');
+const functionUtils = require('./functionUtils');
+const jsonUtils = require('./jsonUtils');
+const logger = require('./logger');
+const mathUtils = require('./mathUtils');
+const numberUtils = require('./numberUtils');
+const objectUtils = require('./objectUtils');
+const pathUtils = require('./pathUtils');
+const rateLimiter = require('./rateLimiter');
+const stringUtils = require('./stringUtils');
+const typeUtils = require('./typeUtils');
+const urlUtils = require('./urlUtils');
+const validationUtils = require('./validationUtils');
+
+module.exports = {
+  ...arrayUtils,
+  ...asyncUtils,
+  ...collectionUtils,
+  ...colorUtils,
+  ...cryptoUtils,
+  ...dateFormatter,
+  ...envUtils,
+  ...fileUtils,
+  ...functionUtils,
+  ...jsonUtils,
+  ...logger,
+  ...mathUtils,
+  ...numberUtils,
+  ...objectUtils,
+  ...pathUtils,
+  ...rateLimiter,
+  ...stringUtils,
+  ...typeUtils,
+  ...urlUtils,
+  ...validationUtils
+};
